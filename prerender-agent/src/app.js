bd2415718c63ffe2ae6bcd305db52a575b779d12
rphansen91 <rphansen91@gmail.com>
2018-11-22 11:26:57 -0800

    Force secure

--- prerender-agent/src/app.js
+++ prerender-agent/src/app.js
@@ -1,12 +1,14 @@
 const { resolve } = require("path");
 const { logger, createMiddlewareLogger } = require("./logger");
 const prerenderConfig = require("./config");
+const forceSecure = require("force-secure-express");
 const prerender = require("prerender-node");
 const express = require("express");
 const app = express();
 
 const port = Number(process.env.PORT) || 8080;
 const public = process.env.PUBLIC || "../public";
+const secureUrls = (process.env.SECURE_URLS || "").split(",").map(s => s.trim());
 const staticPath = resolve(__dirname, public);
 const staticHtml = resolve(staticPath, "index.html");
 
@@ -16,6 +18,8 @@ app.use(prerenderConfig({
   prerenderServiceUrl: process.env.PRERENDER_URL,
   prerenderToken: process.env.PRERENDER_TOKEN
 }, prerender));
+
+app.use(forceSecure(secureUrls));
 
 app.use(express.static(staticPath));
 
